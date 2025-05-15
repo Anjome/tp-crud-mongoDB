052f9f93a50970da28ee67af52592fc9b3305dc5
Angie <angelinamenendez@hotmail.com>
2025-05-15 19:13:53 -0300

    conexion mongoDb fallida

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 
-console.log("Probando")
+import { Schema, model, connect } from "mongoose";
+
+process.loadEnvFile
+
+const URI_DB = process.env.URI_DB || ""
+
+const connectDb = async () => {
+    try {
+        await connect(URI_DB)
+        console.log("Conectado a MongoDB")
+    } catch (error) {
+        console.error("Error al conectar a MongoDB")
+
+    }
+}
+
+connectDb()
