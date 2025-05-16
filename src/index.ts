b5f87282413a55bf29a9ce2225df8653fe4446e1
Angie <angelinamenendez@hotmail.com>
2025-05-15 21:22:10 -0300

    addNewFiesta

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,39 @@ const connectDb = async () => {
     }
 }
 
+interface IFiesta {
+    fecha: string,
+    nombre: string,
+    edad: number,
+    horario: string
+    pago: string
+}
+
 const fiestaSchema = new Schema({
-    fecha: { type: Date, required: true },
+    fecha: { type: String, required: true },
     nombre: { type: String, requerid: true },
     edad: { type: Number },
     horario: { type: String, required: true },
-    Pago: { type: String, requerid: true, enum: ['pendiente', 'señado', 'pagado'], default: 'pendiente' },
+    pago: { type: String, requerid: true, default: 'pendiente' },
 })
 
 const fiesta = model("fiesta", fiestaSchema)
 
-const addNewFiesta = async () => {
+const addNewFiesta = async (newFiesta: IFiesta) => {
     try {
-
-    } catch (error) {
-
+        const { fecha, nombre, edad, horario, pago } = newFiesta
+        if (!fecha || !nombre || !edad || !horario || !pago) {
+            return { succes: false, error: 'Datos invalidos' }
+        }
+        const newFiestaToDb = new fiesta({ fecha, nombre, edad, horario, pago })
+        await newFiestaToDb.save()
+        return {
+            succes: true,
+            data: newFiestaToDb,
+            message: 'Nueva Fiesta'
+        }
+    } catch (error: any) {
+        return { succes: false, error: error.message }
     }
 }
 
@@ -76,10 +94,15 @@ const deleteFiesta = async (id: string) => {
 
 
 
+const main = async () => {
+    connectDb()
+
+    const saveFiesta = await addNewFiesta({ fecha: '11/06/25', nombre: 'Horacio Massare', edad: 56, horario: '18hs', pago: "$50.000" })
+
+    console.log(saveFiesta)
+}
+
+main()
 
 
 
-
-
-
-connectDb()
